919460b2d0ccb97d9d2b794bc73e6c91b507940f
Mertcan Karataş <can.mert.karatas@gmail.com>
2021-10-24 16:03:29 +0300

    first setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CarComponent } from './components/car/car.component';
+import { RentalsComponent } from './components/rentals/rentals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    CarComponent,
+    RentalsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/car.ts
@@ -0,0 +1,8 @@
+export interface Car{
+  brandName:string,
+  colorName:string,
+  carName:string,
+  modelYear:string,
+  dailyPrice:number,
+  description:string
+}
